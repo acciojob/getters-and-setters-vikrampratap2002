4124aa7e17eec8094b4576de0dbcfc624809565c
Vikram pratap <122038154+vikrampratap2002@users.noreply.github.com>
2026-03-02 15:20:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+        this.name = name;
+		this.age = age;
+	}
 
-class Student extends Person {}
+	get name(){
+                    return this.name;
+	}
+	get age(){
+                    return this.age;
+	}
 
-class Teacher extends Person {}
+	set age(age){
+             this.age = age;
+	}
+	set name(name){
+             this.name = name;
+	}
+}
+
+class Student extends Person {
+	study(){
+            console.log(`${name} is studing`);
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+            console.log(`${name} is teaching`);
+	 }
+}
 
 // Do not change the code below this line
 window.Person = Person;
